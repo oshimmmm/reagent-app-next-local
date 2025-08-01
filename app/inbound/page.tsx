1ddb2a851860e30980e44c2eb61147b2fa0b82b7
oshimmmm <yasuhirooshima0419@gmail.com>
2025-08-02 08:55:44 +0900

    軽微な修正

--- app/inbound/page.tsx
+++ app/inbound/page.tsx
@@ -347,7 +347,7 @@ export default function InboundPage() {
 
       {/* 注意書き */}
       <p className="mt-6 text-sm text-gray-600">
-        *Arginase-1, Bond Enzyme Pretreatment, DISH試薬, MSH2, MUC6, PD-L1(SP142)はまだ試薬登録していません。
+        *Arginase-1, Bond Enzyme Pretreatment, ISH Protease 3, MSH2はまだ試薬登録していません。
         上記試薬入庫時は大島を呼んでください。
       </p>
 
